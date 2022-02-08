5951428a53e60639744a509195fcb1f0aae58008
zhihaohe <zhihaohe@autox.ai>
2022-02-08 13:36:00 +0800

    +test workspace

---
+++ test_ws/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, const char ** argv){
+  std::pmr::vector<int> v;
+  v.push_back(1);
+  return 0;
+}
